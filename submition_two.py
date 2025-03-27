681d2d357f99d584b333855c75f9efcd99b07b7e
Eyouel-Alemayehu <joel17alexx@gmail.com>
2025-03-27 13:10:11 +0300

    second comit

---
+++ submition_two.py
@@ -0,0 +1,16 @@
+a="hello World"
+b=str.swapcase(a)
+text="hi i am eyorl and and i love my self"
+f=text.find("a")
+g="hi"
+h=text.index(g)
+print(f"the first ocerance of'{g}'is at index:{h}")
+i="hi"
+j=text.startswith(i)
+print(f"the string {text} starts with:{i} : {j}")
+
+
+print(a)
+print(b)
+print(f)
+
