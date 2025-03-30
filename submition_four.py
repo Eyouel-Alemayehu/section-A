6ea037b549fe6dd2d046c0148129565fcbf61416
Eyouel-Alemayehu <joel17alexx@gmail.com>
2025-03-30 14:26:15 +0300

    FOURTH COMMIT

---
+++ submition_four.py
@@ -0,0 +1,17 @@
+
+
+1.
+text = "   Hello, World!   "
+print(text.lstrip())
+
+2.
+text = "   Hello, World!   "
+print(text.rstrip())
+
+3.
+text = "apple,banana,orange"
+print(text.split(","))
+
+4.
+words = ["apple", "banana", "orange"]
+print(", ".join(words))
