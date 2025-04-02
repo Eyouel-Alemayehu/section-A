10d25f33049ee25a38690924d45f01055c3d70f5
Eyouel-Alemayehu <joel17alexx@gmail.com>
2025-04-02 17:43:35 +0300

    6th commit

---
+++ submition_6.py
@@ -0,0 +1,20 @@
+
+
+1.
+text = "Hello, my name is {} and I am {} years old."
+formatted_text = text.format("Eyouel", 25)
+print(formatted_text)
+
+2.
+name = "Eyouel"
+age = 25
+print(f"Hello, my name is {name} and I am {age} years old.")
+
+3.
+text = "Hello, World!"
+print(len(text))
+
+4.
+text = "Hello, World!"
+encoded_text = text.encode()
+print(encoded_text)
