f6b9f681b3cbe5624005fbbf279154be455f064f
Eyouel-Alemayehu <joel17alexx@gmail.com>
2025-04-09 08:40:29 +0300

    9th upload

---
+++ submition_8.py
@@ -0,0 +1,21 @@
+
+
+1.
+
+fruits = ["apple", "banana"]
+fruits.append("orange")
+print(fruits)
+
+2.
+fruits = ["apple", "banana", "orange"]
+fruits.clear()
+print(fruits)
+
+3.
+fruits = ["apple", "banana", "orange"]
+fruits_copy = fruits.copy()
+print(fruits_copy)
+
+4.
+numbers = [1, 2, 2, 3, 2, 4]
+print(numbers.count(2))
